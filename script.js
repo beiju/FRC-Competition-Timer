c5c40e4dca73f9e87470740d38d01b71525f310d
Will P <beiju@iwillsite.com>
2012-04-10 23:05:56 -0700

    Refactored most of the code. Still far from flawless. Oh, and it doesn't work.

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-var debug = false;
+/** Handle adaptive sizing - as early as possible to avoid FOUC */
+function resize() {
+	window.pxPerEm = (window.innerWidth * 16 / 1440)
+	document.body.style.fontSize = pxPerEm+'px';
+}
+resize();
+window.onresize = function(e) { // I know I should use addEventListener(), but it wasn't working
+	if (typeof resizing != "boolean" || !resizing) {
+		window.resizing = true;
+		resize();
+		resizeTimer();
+		console.warn("Resizing window");
+		window.resizing = false;
+	}
+}
 
-/********** Insert debug values **********/
+
+/** Important Variables */
+var debug = true,
+	timer_main_inner = timer_main.getElementsByClassName('timer')[0],
+	queueing_inner = queueing.getElementsByClassName('timer')[0],
+	playing_inner = playing.getElementsByClassName('timer')[0],
+	judging_inner = judging.getElementsByClassName('timer')[0];
+	
+/** Tempoarry Variables (initialized here for performance) */
+var now, offset, nowQueueingNum, nowPlayingNum, ourNextMatchNum, ourNextMatchTime, i, len, match, time, lowestTimer, target, selected, colorMultiplier, color;
+
+/** Utility Methods */
+function iNaN(val) {
+	return !(val <= 0) && !(val > 0)
+}
+
+
 if (debug) {
 	matchnums.value = '5, 13, 22, 36, 41, 49, 55, 67, 78';
 	known_time.value = '13:35';
 	
-	
+	//! NOTE: not functioning
 	if (window.applicationCache.status != window.applicationCache.UNCACHED) {
 		window.applicationCache.update();
 		
@@ -29,31 +59,29 @@ if (debug) {
 	}
 }
 
-/********** Handle adaptive sizing **********/
-function resize() {
-	window.pxPerEm = (window.innerWidth * 16 / 1440)
-	document.body.style.fontSize = pxPerEm+'px';
-}
-resize();
-window.onresize = function(e) { // I know I should use addEventListener(), but it wasn't working
-	if (typeof resizing != "boolean" || !resizing) {
-		window.resizing = true;
-		resize();
-		resizeTimer();
-		console.warn("Resizing window");
-		window.resizing = false;
-	}
-}
 
-/********** Get and update values of inputs **********/
+
+
+/** Get and update values of inputs */
 var interval = parseFloat(match_interval.value, 10)*60,
 	color_change_time = parseFloat(change_color.value, 10),
 	matches = matchnums.value.match(/\d+/g),
-	knownNum = parseInt(known_num.value, 10), // Without the 10, it assumes numbers prepended with 0 are octal
-	knownTime = {
+	referenceMatchNum = parseInt(known_num.value, 10), // Without the 10, it assumes numbers prepended with 0 are octal
+	referenceMatchTime = {
 		hours: parseInt(known_time.value.match(/^\d+/)[0], 10),
 		minutes: parseInt(known_time.value.match(/\d+$/)[0], 10)
-	}
+	}, 
+	judgingTime = {
+		hours: parseInt(judging_time.value.match(/^\d+/)[0], 10),
+		minutes: parseInt(judging_time.value.match(/\d+$/)[0], 10)
+	};
+	
+/* Idea: eliminate code repetition, set event listeners dynamically
+var inputs = [
+	{ id: 'match_interval', variable: 'interval' },
+	{ id: 'change_color', variable: 'color_change_time' },
+	{ id: 'matchnums', variable: 'matches' },
+	*/
 	
 match_interval.addEventListener('change', function(event) {
 	interval = parseFloat(match_interval.value, 10)*60;
@@ -65,111 +93,32 @@ matchnums.addEventListener('change', function(event) {
 	matches = matchnums.value.match(/\d+/g);
 }, false)
 known_num.addEventListener('change', function(event) {
-	knownNum = parseInt(known_num.value, 10);
+	referenceMatchNum = parseInt(known_num.value, 10);
 }, false)
 known_time.addEventListener('change', function(event) {
-	knownTime = {
-		hours: parseInt(known_time.value.match(/^\d+/)[0], 10),
-		minutes: parseInt(known_time.value.match(/\d+$/)[0], 10)
-	}
+	referenceMatchTime.hours = parseInt(known_time.value.match(/^\d+/)[0], 10);
+	referenceMatchTime.minutes = parseInt(known_time.value.match(/\d+$/)[0], 10);
+}, false)
+judging_time.addEventListener('change', function(event) {
+	judgingTime.hours = parseInt(judging_time.value.match(/^\d+/)[0], 10);
+	judgingTime.minutes = parseInt(judging_time.value.match(/\d+$/)[0], 10);
 }, false)
 
 
-/********** Code for "Now" button **********/
+/** Code for "Now" button */
 now.addEventListener('click', function(event) { 
 	var now = new Date(), 
 		hour = now.getHours(),
 		minute = now.getMinutes();
 		
 	known_time.value = (hour<10?'0':'')+hour + ':' + (minute<10?'0':'')+minute;
-	knownTime.hours = hour;
-	knownTime.minutes = minute;
+	referenceMatchTime.hours = hour;
+	referenceMatchTime.minutes = minute;
 }, false)
 
-/********** Listen for Judges' award **********/
-function judgeTime() {
-	if (judge_time.value == '' || !judge_time.value.match(/[1-9]/) || judge_today.value == "false" ) {
-		if (!judging.className.match(/hidden/)) {
-			judging.className += ' hidden';
-			judging.getElementsByClassName('timer')[0].removeAttribute('data-time');
-		}
-		return;
-	} else {
-		if (judging.className.match(/hidden/)) judging.className.replace(' hidden', '');
-	}
-	
-	var time  = {
-		hours: parseInt(judge_time.value.match(/^\d+/)[0], 10),
-		minutes: parseInt(judge_time.value.match(/\d+$/)[0], 10)
-	}
-	
-}
-judgeTime();
-judge_time.addEventListener('change', judgeTime, false);
-judge_today.addEventListener('change', judgeTime, false);
-
-/********** Hash change listener **********/
-function switchTimers(event) {
-	var target = document.location.hash.replace('#','');
-	
-	if (target == "") target = "auto";
-	
-	timer_main.className = target;
-	
-	if (target == "auto") {
-		window.autoSelectTimerInterval = setInterval(autoSelectTimer, 1000);
-		window.oldLowestID = '';
-		autoSelectTimer();
-	} else if (typeof autoSelectTimerInterval != "undefined") {
-		clearInterval(autoSelectTimerInterval);
 		
-		var selected = timer_list.getElementsByClassName('selected')
-		for (var i = 0, len = selected.length; i < len; i++) {
-			selected[i].className = selected[i].className.replace(/ ?selected/gi, '');
-		}
-	}
-	
-	if (document.getElementById(target).className.match(/hidden/)) {
-		document.location.hash = 'auto';
-	}
-	
-	updateTime();
-}
-switchTimers();
-window.addEventListener('hashchange', function (event) {
-	switchTimers();
-	event.preventDefault();
-}, false);
 
-function autoSelectTimer() {
-	var elements = timer_list.getElementsByClassName('timer'),
-		lowest, lowest_i = 0;
-	
-	for (var i=0, len = elements.length; i<len; i++) {
-		if (!elements[i].getAttribute('data-time')) continue;
-		var time = parseFloat(elements[i].getAttribute('data-time'));
-		if (typeof lowest != "number" || (time < lowest && time != 0)) {
-			lowest = time;
-			lowest_i = i;
-		}
-	}
-	
-	var lowestID = elements[lowest_i].parentNode.parentNode.getAttribute('id');
-	
-	if (typeof oldLowestID != "string" || lowestID != oldLowestID) {
-		timer_main.className = lowestID;
-		var selected = timer_list.getElementsByClassName('selected')
-		for (var i = 0, len = selected.length; i < len; i++) {
-			selected[i].innerHTML = selected[i].innerHTML.replace(/ ?selected/i, '');
-		}
-		document.getElementById(lowestID).className += ' selected';
-		updateTime();
-		
-		window.oldLowestID = lowestID;
-	}
-}		
-
-/********** Class for handling times **********/
+/** Class for handling times (deprecated) */
 function Time(minutes) {
 	var o = {
 		time: minutes,
@@ -201,17 +150,158 @@ function Time(minutes) {
 	return o;
 }
 
-/********** Display timer values **********/
-function displayTime(time, id) {
-	var elem = document.getElementById(id).getElementsByClassName('timer')[0];
-	elem.innerHTML = time.formatted;
-	elem.setAttribute('data-time', time.time);
-	
-	if (timer_main.className.match(id)) {
-		displayTime(time, 'timer_main');
-		resizeTimer();
+/** Class to store a timer. Time is stored in minutes. Heavily optimized, but optimized for the wrong uses.
+ * It should work well for different uses, but will probably have to be rewritten again for this use.
+ */
+function Timer(time) {
+	return {
+		time: time,
+		c: { // shortened version of 'cache'
+			days: undefined,
+			hours: undefined,
+			minutes: undefined,
+			seconds: undefined,
+			negative: undefined,
+			formatted: undefined
+		},
+		
+		// Stored for performance
+		neg_sign: '-',
+		separator: ':',
+		zero: '0',
+		tnd_error: "Time Not Defined Error",
+		
+		days: function() {
+			if (this.time == undefined) throw tnd_error;
+			if (this.c.days == undefined)  this.c.days = Math.abs( ~~(this.time / 60 / 24) );
+			return this.c.days;
+		},
+		hours: function() {
+			if (this.time == undefined) throw tnd_error;
+			if (this.c.hours == undefined) this.c.hours = Math.abs( ~~(this.time / 60) );
+			return this.c.hours;
+		},
+		minutes: function() {
+			if (this.time == undefined) throw tnd_error;
+			if (this.c.minutes == undefined) this.c.minutes = Math.abs( ~~this.time);
+			return this.c.minutes;
+		},
+		seconds: function() {
+			if (this.time == undefined) throw tnd_error;
+			if (this.c.seconds == undefined) this.c.seconds = Math.abs( ~~(this.time*60) );
+			return this.c.seconds;
+		},
+		negative: function() {
+			if (this.time == undefined) throw tnd_error;
+			if (this.c.negative == undefined)  this.c.negative = (this.time < 0);
+			return this.c.negative;
+		},
+		
+		formatted: function() {
+			if (this.time == undefined) throw tnd_error;
+			this.c.formatted = '';
+			
+			if (this.negative()) this.c.formatted += this.neg_sign;
+			if (this.days() != 0) this.c.formatted += this.days()+this.separator;
+			if (this.hours() != 0) this.c.formatted += this.hours()+this.separator;
+			if (this.minutes() < 10) this.c.formatted += this.zero;
+			this.c.formatted += this.minutes()+this.separator;
+			if (this.seconds() < 10) this.c.formatted += this.zero;
+			this.c.formatted += this.seconds();
+			
+			return this.c.formatted;
+		},
+		
+		changeTo: function(time) {
+			this.time = time;
+			
+			this.c.days = undefined;
+			this.c.hours = undefined;
+			this.c.minutes = undefined;
+			this.c.seconds = undefined;
+			this.c.negative = undefined;
+			this.c.formatted = undefined;
+		}
+		
 	}
 }
+var queueingTimer = new Timer(), playingTimer = new Timer(), judgingTimer = new Timer(), timers = [queueingTimer, playingTimer, judgingTimer], mainTimer;
+
+
+/** Hash change listener */
+function switchTimers(event) {
+	target = document.location.hash.replace('#','');
+	
+	if (target == "") {
+		document.location.hash = "auto";
+		return; // this function will be triggered again
+	}
+	
+	timer_main.className = target;
+	
+	if (target != "auto" && typeof autoSelectedTimerInterval != "undefined") {
+		selected = timer_list.getElementsByClassName('selected')
+		for (i = 0, len = selected.length; i < len; i++) {
+			selected[i].classList.remove('selected');
+		}
+		mainTimer = window[target+'Timer'];
+	}
+	
+	if (document.getElementById(target).className.match(/hidden/)) { // If the picked timer is hidden, for some reason
+		document.location.hash = 'auto';
+	}
+}
+switchTimers();
+window.addEventListener('hashchange', function (event) {
+	switchTimers();
+	event.preventDefault();
+}, false);
+
+
+
+/** Utility: Set configured/enabled states */
+function setConfigured(configured) {
+	// configured state sorts itself out automatically
+	if (!configured) {
+		timer_main_inner.innerHTML = 'Not Configured';
+		resizeTimer();
+		document.body.style.backgroundColor = '';
+	}
+}
+function setJudgingEnabled(enabled) {
+	if (!enabled) {
+		judging.classList.add('hidden');
+		judging_inner.removeAttribute('data-time');
+	} else {
+		judging.classList.remove('disabled');
+	}
+}
+
+
+/** Display timer values */
+function displayTimers() {
+	if (!isNaN(queueingTimer.time)) queueing_inner.innerHTML = queueingTimer.formatted();
+	if (!isNaN(playingTimer.time)) playing_inner.innerHTML = playingTimer.formatted();
+	if (!isNaN(judgingTimer.time)) judging_inner.innerHTML = judgingTimer.formatted();
+	if (mainTimer != undefined && !isNaN(mainTimer.time)) timer_main_inner.innerHTML = mainTimer.formatted();
+	
+	
+}
+
+/** Display color */
+function displayColors() {
+	colorMultiplier = mainTimer.time/color_change_time;
+	if (colorMultiplier > 1) colorMultiplier = 1;
+	else if (colorMultiplier < 0) colorMultiplier = 0;
+	
+	color = 'hsl('+Math.round(130*colorMultiplier)+',100%,50%)';
+	
+	if (typeof oldColor != "string" || oldColor != color) {
+		document.body.style.backgroundColor = color;
+		window.oldColor = color;
+	}
+}
+
 
 function resizeTimer() {
 	var timerElem = timer_main.getElementsByClassName('timer')[0],
@@ -226,7 +316,108 @@ function resizeTimer() {
 	
 }
 
-/********** Handle updating time **********/
+/** Handle updating time */
+
+function updateMatchTime() {
+	// Detect if required data has been supplied
+	if (matches == null || iNaN(interval) || iNaN(referenceMatchNum) || iNaN(referenceMatchTime.hours) || iNaN(referenceMatchTime.minutes)) {
+		setConfigured(false);
+		return;
+	} else { 
+		setConfigured(true);
+	}
+	
+	now = new Date();
+	
+	offset = (now.getHours()*60*60+now.getMinutes()*60+now.getSeconds()) - (referenceMatchTime.hours*60*60+referenceMatchTime.minutes*60);
+	nowQueueingNum = referenceMatchNum + offset / interval;
+	nowPlayingNum = nowQueueingNum - 3; //! TODO: Make this configurable
+	ourNextMatchNum = 0;
+	
+	for (len = matches.length, i = 0; i < len && ourNextMatchNum == 0; i++) {
+		match = parseInt(matches[i], 10);
+		if (match > nowPlayingNum) ourNextMatchNum = match;
+	}
+	//! TODO: Add detection of erroneous match numbers here: competition is over, it's between days, it's lunchtime, etc.
+	if (ourNextMatchNum == 0) ourNextMatchNum = match; // Band-aid
+	
+	nowQueueingNum = ourNextMatchNum - nowQueueingNum;
+	playingNum = ourNextMatchNum - nowPlayingNum;
+	queueingTimer.changeTo(nowQueueingNum*interval/60); 
+	playingTimer.changeTo(nowPlayingNum*interval/60); 
+
+}
+function updateJudgingTime() {
+	// Detect if required data has been supplied
+	if (iNaN(judgingTime) || !judgeToday || judgingTime.minutes == 0) {
+		setJudgingEnabled(false);
+		return;
+	} else {
+		setJudgingEnabled(true);
+	}
+	
+	now = new Date();
+	time = (judgeToday.hours - now.getHours()) * 60 + (judgeToday.minutes - now.getMinutes()) - now.getSeconds();
+	judgingTimer.changeTo(time);
+}
+function updateAutoSelectedTimer() {
+	lowestTimer = 0;
+	for (i = 0, len = timers.length; i < len; i++) {
+		if (timers[i].time < timers[lowestTimer].time) lowestTimer = i;
+	}
+	mainTimer = timers[lowestTimer];
+}
+
+/*
+function autoSelectedTimer() {
+	var elements = timer_list.getElementsByClassName('timer'),
+		lowest, lowest_i = 0;
+	
+	for (var i=0, len = elements.length; i<len; i++) {
+		if (!elements[i].getAttribute('data-time')) continue;
+		var time = parseFloat(elements[i].getAttribute('data-time'));
+		if (typeof lowest != "number" || (time < lowest && time != 0)) {
+			lowest = time;
+			lowest_i = i;
+		}
+	}
+	
+	var lowestID = elements[lowest_i].parentNode.parentNode.getAttribute('id');
+	
+	if (typeof oldLowestID != "string" || lowestID != oldLowestID) {
+		timer_main.className = lowestID;
+		var selected = timer_list.getElementsByClassName('selected')
+		for (var i = 0, len = selected.length; i < len; i++) {
+			selected[i].innerHTML = selected[i].innerHTML.replace(/ ?selected/i, '');
+		}
+		document.getElementById(lowestID).className += ' selected';
+		
+		window.oldLowestID = lowestID;
+	}
+}
+
+/** Listen for Judges' award ** /
+function judgingTime() {
+	if (judging_time.value == '' || !judging_time.value.match(/[1-9]/) || judge_today.value == "false" ) {
+		if (!judging.className.match(/hidden/)) {
+			judging.className += ' hidden';
+			judging.getElementsByClassName('timer')[0].removeAttribute('data-time');
+		}
+		return;
+	} else {
+		if (judging.className.match(/hidden/)) judging.className.replace(' hidden', '');
+	}
+	
+	var time  = {
+		hours: parseInt(judging_time.value.match(/^\d+/)[0], 10),
+		minutes: parseInt(judging_time.value.match(/\d+$/)[0], 10)
+	}
+	
+}
+judgingTime();
+judging_time.addEventListener('change', judgingTime, false);
+judge_today.addEventListener('change', judgingTime, false);
+
 function updateTime() {
 	var now = new Date();
 		
@@ -237,8 +428,8 @@ function updateTime() {
 		return;
 	}
 	
-	var offset = (now.getHours()*60*60+now.getMinutes()*60+now.getSeconds()) - (knownTime.hours*60*60+knownTime.minutes*60),
-		currentlyQueueing = knownNum + offset / interval,
+	var offset = (now.getHours()*60*60+now.getMinutes()*60+now.getSeconds()) - (referenceMatchTime.hours*60*60+referenceMatchTime.minutes*60),
+		currentlyQueueing = referenceMatchNum + offset / interval,
 		currentlyPlaying = currentlyQueueing - 3,
 		ourNextNum = 0;
 	
@@ -248,10 +439,6 @@ function updateTime() {
 	}
 	if (ourNextNum == 0) ourNextNum = match;
 	
-	var queueingNum = ourNextNum - currentlyQueueing,
-		playingNum = ourNextNum - currentlyPlaying,
-		queueingTime = new Time(queueingNum*interval/60), // input is in minutes
-		playingTime = new Time(playingNum*interval/60); // input is in minutes
 	
 	our_next.getElementsByClassName('replace')[0].innerHTML = ourNextNum;
 	now_queueing.getElementsByClassName('replace')[0].innerHTML = Math.floor(currentlyQueueing);
@@ -274,7 +461,7 @@ function updateTime() {
 	
 	if (debug && false) {
 		console.log("Now:", now);
-		console.log("Last Known time:", knownTime.hours, ':', knownTime.minutes);
+		console.log("Last Known time:", referenceMatchTime.hours, ':', referenceMatchTime.minutes);
 		console.log("Offset between now and last known time:", offset);
 		console.log("Currently queueing match:", currentlyQueueing, "Currently playing match:", currentlyPlaying);
 		console.log("Next match we play:", ourNextNum);
@@ -286,7 +473,14 @@ function updateTime() {
 		console.log('color: "'+color+'"');
 	}
 }
-updateTime();
-setInterval(function() {
-	updateTime();
-}, 1000);
+updateTime();*/
+
+/** Master Interval */
+function masterInterval() {
+	updateMatchTime();
+	updateJudgingTime();
+	updateAutoSelectedTimer();
+	displayTimers();
+	displayColors();
+}
+window.setInterval(masterInterval, 1000)
